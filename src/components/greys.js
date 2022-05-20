418959066eef8995f717c5fa8e3cd0e452051bd9
cmilne78 <79056900+cmilne78@users.noreply.github.com>
2022-05-20 19:01:02 -0400

    pushing from local

--- src/components/greys.js
+++ src/components/greys.js
@@ -3,15 +3,149 @@ import styled from "styled-components"
 
 const Greys = () => (
   <Wrapper>
-    <svg
-      width="122"
-      height="122"
-      viewBox="0 0 122 122"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="60.8889" cy="60.8889" r="60.8889" fill="#ffffff" />
-    </svg>
+    <SvgWrapper>
+      <svg
+        width="92"
+        height="92"
+        viewBox="0 0 92 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="45.8318" cy="45.8889" r="45.2317" fill="#1C1E1F" />
+      </svg>
+      <SvgText>900</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="92"
+        height="92"
+        viewBox="0 0 92 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="46.1238" cy="45.8889" r="45.2317" fill="#2F3233" />
+      </svg>
+      <SvgText>800</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="91"
+        height="92"
+        viewBox="0 0 91 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="45.4158" cy="45.8889" r="45.2317" fill="#6D7173" />
+      </svg>
+      <SvgText>700</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="91"
+        height="92"
+        viewBox="0 0 91 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="45.7083" cy="45.8889" r="45.2317" fill="#84888A" />
+      </svg>
+      <SvgText>600</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="92"
+        height="92"
+        viewBox="0 0 92 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="46.0003"
+          cy="45.8889"
+          r="44.8603"
+          fill="#B3B8BA"
+          stroke="#84888A"
+          stroke-width="0.742857"
+        />
+      </svg>
+      <SvgText>500</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="91"
+        height="92"
+        viewBox="0 0 91 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="45.2918"
+          cy="45.8889"
+          r="44.8603"
+          fill="#D0D4D6"
+          stroke="#B3B8BA"
+          stroke-width="0.742857"
+        />
+      </svg>
+      <SvgText>400</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="91"
+        height="92"
+        viewBox="0 0 91 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="45.5838"
+          cy="45.8889"
+          r="44.8603"
+          fill="#E4E8EB"
+          stroke="#D0D4D6"
+          stroke-width="0.742857"
+        />
+      </svg>
+      <SvgText>300</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="92"
+        height="92"
+        viewBox="0 0 92 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="45.8763"
+          cy="45.8889"
+          r="44.8603"
+          fill="#F0F5F7"
+          stroke="#E3E8EB"
+          stroke-width="0.742857"
+        />
+      </svg>
+      <SvgText>200</SvgText>
+    </SvgWrapper>
+    <SvgWrapper>
+      <svg
+        width="92"
+        height="92"
+        viewBox="0 0 92 92"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="46.1683"
+          cy="45.8889"
+          r="44.8603"
+          fill="#FAFCFC"
+          stroke="#F0F5F7"
+          stroke-width="0.742857"
+        />
+      </svg>
+      <SvgText>100</SvgText>
+    </SvgWrapper>
   </Wrapper>
 )
 
@@ -21,11 +155,23 @@ const Wrapper = styled.div`
   align-items: flex-start;
   align-items: center;
   background-color: white;
-  width: auto;
+  max-width: 1040px;
   height: auto;
   padding: 40px;
-  gap: 40px;
+  gap: 16px;
   border-radius: 15px;
+  margin: 120px;
 `
-
+const SvgWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  align-items: center;
+  max-width: 1040px;
+`
+const SvgText = styled.p`
+  color: black;
+  font-size: 14px;
+  font-weight: 600;
+`
 export default Greys
---
+++ src/components/theme.js
@@ -0,0 +1,25 @@
+// example theme
+export const theme = {
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "#009ADD",
+    secondary: "#05a",
+    accent: "#609",
+    muted: "#f6f6f6",
+  },
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: "inter, sans-serif",
+    monospace: "Menlo, monospace",
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+}
